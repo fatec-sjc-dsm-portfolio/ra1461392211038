5d020ffc51d210dbb0aa281dc7fbbdee008ff22f
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2025-06-02 16:21:17 -0300

    Reformulação do projetos e e responsividade.

--- docs/script.js
+++ docs/script.js
@@ -1,39 +1,15 @@
-const carrossel = document.getElementById('carrossel');
-const btnLeft = document.getElementById('btn-left');
-const btnRight = document.getElementById('btn-right');
-
-let index = 0;
-const totalItems = document.querySelectorAll('.projeto-item').length;
-
-btnLeft.addEventListener('click', () => {
-    if (index > 0) {
-        index--;
-        updateCarrossel();
-    }
-});
-
-btnRight.addEventListener('click', () => {
-    if (index < totalItems - 1) {
-        index++;
-        updateCarrossel();
-    }
-});
-
-function updateCarrossel() {
-    const translateX = index * -100;
-    carrossel.style.transform = `translateX(${translateX}%)`;
-}
-
 const texts = ["Fullstack", "Back-end", "Front-end"];
 const dynamicText = document.getElementById("dynamic-text");
 const cursor = document.getElementById("cursor");
+
+let index = 0;
 
 function typeText(text, i, callback) {
   if (i < text.length) {
     dynamicText.textContent += text.charAt(i);
     setTimeout(() => typeText(text, i + 1, callback), 100);
   } else {
-    setTimeout(callback, 1000); // Espera 1 segundo antes de começar a apagar
+    setTimeout(callback, 1000);
   }
 }
 
@@ -42,18 +18,16 @@ function eraseText(text, i, callback) {
     dynamicText.textContent = text.substring(0, i);
     setTimeout(() => eraseText(text, i - 1, callback), 100);
   } else {
-    setTimeout(callback, 500); // Espera 0.5 segundos antes de mudar o texto
+    setTimeout(callback, 500);
   }
 }
 
 function changeText() {
   const currentText = texts[index];
-  index = (index + 1) % texts.length; // Alterna entre 0 e 1
-  eraseText(currentText, currentText.length - 1, () => typeText(texts[index], 0, changeText)); // Apaga e então escreve a nova palavra
+  index = (index + 1) % texts.length;
+  eraseText(currentText, currentText.length - 1, () => typeText(texts[index], 0, changeText));
 }
 
-// Começa a animação com a primeira palavra
 typeText(texts[index], 0, () => {
-  // Uma vez que a primeira palavra foi digitada, inicia a troca
   changeText();
 });
